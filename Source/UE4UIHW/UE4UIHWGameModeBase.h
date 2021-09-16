a9f566b73f647fb4761384dcd6c1f987ae8f4096
JoeTesla <joean@mail.ru>
2021-09-16 17:17:50 +0300

    Creating a project with a character and base moving skills

---
+++ Source/UE4UIHW/UE4UIHWGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "UE4UIHWGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UE4UIHW_API AUE4UIHWGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
